dd7fb0d427de559be071c240b884f32c6f0138a1
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-09-29 07:06:33 +0900

    build.gradle.ktsの変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.10"
 }
-group = "azkz.com"
+group = "com.azkz"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 dependencies {
-    testImplementation(kotlin("test-junit5"))
     implementation("org.apache.commons:commons-lang3:3.11")
     implementation("com.opencsv:opencsv:5.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
