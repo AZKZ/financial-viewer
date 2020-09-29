2d74eb5d6b67d2a1f1bb4385c24ebf7b36b735b8
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-09-30 06:50:24 +0900

    build.gradle.ktsの更新 テスト関係の記述を変えた

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,23 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.11")
     implementation("com.opencsv:opencsv:5.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
