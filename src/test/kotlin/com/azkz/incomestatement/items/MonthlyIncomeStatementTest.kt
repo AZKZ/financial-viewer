ecdad2b8d45612455b00aecf2ed37f1ec4bb25a1
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-10-04 16:05:56 +0900

    コメントとテストの追加

--- src/test/kotlin/com/azkz/incomestatement/items/MonthlyIncomeStatementTest.kt
+++ src/test/kotlin/com/azkz/incomestatement/items/MonthlyIncomeStatementTest.kt
@@ -6,7 +6,6 @@ import com.azkz.domain.accountitle.AccountTitle
 import com.azkz.domain.incomestatement.impl.MonthlyIncomeStatementImpl
 import com.azkz.domain.value.Amount
 import com.azkz.domain.value.Currency
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import java.time.YearMonth
 
@@ -17,32 +16,76 @@ class MonthlyIncomeStatementTest {
 
     companion object {
         val YEAR_MONTH = YearMonth.parse("2020-05")
-        val SALES_BREAKDOWN = MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.SALES, "売上の内訳"))
-        val COST_OF_SALES_BREAKDOWN =
-            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.COST_OF_SALES, "売上原価の内訳"))
-        val SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN = MockMonthlyAccountBreakdown(
-            YEAR_MONTH,
-            AccountTitle(AccountSubcategory.SELLING_GENERAL_ADMINISTRATIVE_EXPENSE, "販売管理費の内訳")
-        )
-        val NON_OPERATING_INCOME_BREAKDOWN =
-            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.NON_OPERATING_INCOME, "営業外収益の内訳"))
-        val NON_OPERATING_EXPENSE_BREAKDOWN =
-            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.NON_OPERATING_EXPENSE, "営業外費用の内訳"))
-        val EXTRAORDINARY_INCOME_BREAKDOWN =
-            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.EXTRAORDINARY_INCOME, "特別損失の内訳"))
-        val EXTRAORDINARY_LOSS_BREAKDOWN =
-            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.EXTRAORDINARY_LOSS, "特別損失の内訳"))
 
+        /** 売上の内訳 */
+        val SALES_BREAKDOWN1 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.SALES, "売上の内訳1"))
+        val SALES_BREAKDOWN2 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.SALES, "売上の内訳2"))
+
+        /** 売上原価の内訳 */
+        val COST_OF_SALES_BREAKDOWN1 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.COST_OF_SALES, "売上原価の内訳1"))
+        val COST_OF_SALES_BREAKDOWN2 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.COST_OF_SALES, "売上原価の内訳2"))
+
+        /** 販売管理費の内訳 */
+        val SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN1 =
+            MockMonthlyAccountBreakdown(
+                YEAR_MONTH,
+                AccountTitle(AccountSubcategory.SELLING_GENERAL_ADMINISTRATIVE_EXPENSE, "販売管理費の内訳1")
+            )
+        val SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN2 =
+            MockMonthlyAccountBreakdown(
+                YEAR_MONTH,
+                AccountTitle(AccountSubcategory.SELLING_GENERAL_ADMINISTRATIVE_EXPENSE, "販売管理費の内訳2")
+            )
+
+        /** 営業外収益の内訳 */
+        val NON_OPERATING_INCOME_BREAKDOWN1 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.NON_OPERATING_INCOME, "営業外収益の内訳1"))
+        val NON_OPERATING_INCOME_BREAKDOWN2 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.NON_OPERATING_INCOME, "営業外収益の内訳2"))
+
+        /** 営業外費用の内訳 */
+        val NON_OPERATING_EXPENSE_BREAKDOWN1 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.NON_OPERATING_EXPENSE, "営業外費用の内訳1"))
+        val NON_OPERATING_EXPENSE_BREAKDOWN2 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.NON_OPERATING_EXPENSE, "営業外費用の内訳2"))
+
+        /** 特別利益の内訳 */
+        val EXTRAORDINARY_INCOME_BREAKDOWN1 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.EXTRAORDINARY_INCOME, "特別利益の内訳1"))
+        val EXTRAORDINARY_INCOME_BREAKDOWN2 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.EXTRAORDINARY_INCOME, "特別利益の内訳2"))
+
+        /** 特別損失の内訳 */
+        val EXTRAORDINARY_LOSS_BREAKDOWN1 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.EXTRAORDINARY_LOSS, "特別損失の内訳1"))
+        val EXTRAORDINARY_LOSS_BREAKDOWN2 =
+            MockMonthlyAccountBreakdown(YEAR_MONTH, AccountTitle(AccountSubcategory.EXTRAORDINARY_LOSS, "特別損失の内訳2"))
+
+        /** 正常な内訳の集合 */
         val BREAKDOWNS =
             setOf<MonthlyAccountBreakdown>(
-                SALES_BREAKDOWN,
-                COST_OF_SALES_BREAKDOWN,
-                SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN,
-                NON_OPERATING_INCOME_BREAKDOWN,
-                NON_OPERATING_EXPENSE_BREAKDOWN,
-                EXTRAORDINARY_INCOME_BREAKDOWN,
-                EXTRAORDINARY_LOSS_BREAKDOWN
+                SALES_BREAKDOWN1,
+                COST_OF_SALES_BREAKDOWN1,
+                SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN1,
+                NON_OPERATING_INCOME_BREAKDOWN1,
+                NON_OPERATING_EXPENSE_BREAKDOWN1,
+                EXTRAORDINARY_INCOME_BREAKDOWN1,
+                EXTRAORDINARY_LOSS_BREAKDOWN1,
+                SALES_BREAKDOWN2,
+                COST_OF_SALES_BREAKDOWN2,
+                SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN2,
+                NON_OPERATING_INCOME_BREAKDOWN2,
+                NON_OPERATING_EXPENSE_BREAKDOWN2,
+                EXTRAORDINARY_INCOME_BREAKDOWN2,
+                EXTRAORDINARY_LOSS_BREAKDOWN2,
             )
+
+        /** 空の内訳の集合 */
+        val EMPTY_BREAKDOWNS = emptySet<MonthlyAccountBreakdown>()
 
 
     }
@@ -50,19 +93,92 @@ class MonthlyIncomeStatementTest {
     @Test
     fun `Test to create`() {
         val monthlyIncomeStatement = MonthlyIncomeStatementImpl(YEAR_MONTH, BREAKDOWNS)
-        assertNotNull(monthlyIncomeStatement)
 
-        assert(monthlyIncomeStatement.sales.breakdowns.contains(SALES_BREAKDOWN))
-        assert(monthlyIncomeStatement.costOfSales.breakdowns.contains(COST_OF_SALES_BREAKDOWN))
+        // それぞれの内訳に対応する項目に格納されていることを確認
         assert(
-            monthlyIncomeStatement.sellingGeneralAdministrativeExpenses.breakdowns.contains(
-                SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN
+            monthlyIncomeStatement.sales.breakdowns.contains(
+                SALES_BREAKDOWN1,
             )
         )
-        assert(monthlyIncomeStatement.nonOperatingIncome.breakdowns.contains(NON_OPERATING_INCOME_BREAKDOWN))
-        assert(monthlyIncomeStatement.nonOperatingExpenses.breakdowns.contains(NON_OPERATING_EXPENSE_BREAKDOWN))
-        assert(monthlyIncomeStatement.extraordinaryIncome.breakdowns.contains(EXTRAORDINARY_INCOME_BREAKDOWN))
-        assert(monthlyIncomeStatement.extraordinaryLoss.breakdowns.contains(EXTRAORDINARY_LOSS_BREAKDOWN))
+        assert(
+            monthlyIncomeStatement.sales.breakdowns.contains(
+                SALES_BREAKDOWN2,
+            )
+        )
+        assert(
+            monthlyIncomeStatement.costOfSales.breakdowns.contains(
+                COST_OF_SALES_BREAKDOWN1
+            )
+        )
+        assert(
+            monthlyIncomeStatement.costOfSales.breakdowns.contains(
+                COST_OF_SALES_BREAKDOWN2
+            )
+        )
+        assert(
+            monthlyIncomeStatement.sellingGeneralAdministrativeExpenses.breakdowns.contains(
+                SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN1
+            )
+        )
+        assert(
+            monthlyIncomeStatement.sellingGeneralAdministrativeExpenses.breakdowns.contains(
+                SELLING_GENERAL_ADMINISTRATIVE_EXPENSE_BREAKDOWN2
+            )
+        )
+        assert(
+            monthlyIncomeStatement.nonOperatingIncome.breakdowns.contains(
+                NON_OPERATING_INCOME_BREAKDOWN1
+            )
+        )
+        assert(
+            monthlyIncomeStatement.nonOperatingIncome.breakdowns.contains(
+                NON_OPERATING_INCOME_BREAKDOWN2
+            )
+        )
+        assert(
+            monthlyIncomeStatement.nonOperatingExpenses.breakdowns.contains(
+                NON_OPERATING_EXPENSE_BREAKDOWN1
+            )
+        )
+        assert(
+            monthlyIncomeStatement.nonOperatingExpenses.breakdowns.contains(
+                NON_OPERATING_EXPENSE_BREAKDOWN2
+            )
+        )
+        assert(
+            monthlyIncomeStatement.extraordinaryIncome.breakdowns.contains(
+                EXTRAORDINARY_INCOME_BREAKDOWN1
+            )
+        )
+        assert(
+            monthlyIncomeStatement.extraordinaryIncome.breakdowns.contains(
+                EXTRAORDINARY_INCOME_BREAKDOWN2
+            )
+        )
+        assert(
+            monthlyIncomeStatement.extraordinaryLoss.breakdowns.contains(
+                EXTRAORDINARY_LOSS_BREAKDOWN1
+            )
+        )
+        assert(
+            monthlyIncomeStatement.extraordinaryLoss.breakdowns.contains(
+                EXTRAORDINARY_LOSS_BREAKDOWN2
+            )
+        )
+    }
+
+    @Test
+    fun `Test to create by empty breakdowns`() {
+        val monthlyIncomeStatement = MonthlyIncomeStatementImpl(YEAR_MONTH, EMPTY_BREAKDOWNS)
+
+        /** 各項目の内訳が空でできていることを確認 */
+        assert(monthlyIncomeStatement.sales.breakdowns.isEmpty())
+        assert(monthlyIncomeStatement.costOfSales.breakdowns.isEmpty())
+        assert(monthlyIncomeStatement.sellingGeneralAdministrativeExpenses.breakdowns.isEmpty())
+        assert(monthlyIncomeStatement.nonOperatingIncome.breakdowns.isEmpty())
+        assert(monthlyIncomeStatement.nonOperatingExpenses.breakdowns.isEmpty())
+        assert(monthlyIncomeStatement.extraordinaryIncome.breakdowns.isEmpty())
+        assert(monthlyIncomeStatement.extraordinaryLoss.breakdowns.isEmpty())
     }
 
 
