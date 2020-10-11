12cd99e42fcf9a6a9e859db1fc5eeef18f59a564
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-10-11 22:32:06 +0900

    amplify authの追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
+    <amplify-authenticator></amplify-authenticator>
+    <amplify-sign-out></amplify-sign-out>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <router-link to="/chart">Chart</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import awsExports from './aws-exports'
+
+Amplify.configure(awsExports);
 
 Vue.config.productionTip = false
 
