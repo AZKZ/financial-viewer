3ce1c29e1b083262f7e54c990d0a2106c3b03cef
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-10-11 21:31:34 +0900

    Chartページの作成

---
+++ frontend/src/components/FileUpload.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="file-upload">
+    <form method="post" action="">
+      <input type="file">
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class FileUpload extends Vue {
+  @Prop() private fileName!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
