888af7bdd554d490375d99fecedccbcc9cd61698
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-10-12 21:49:42 +0900

    S3ファイルアップロード機能の実装

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@
     <amplify-sign-out></amplify-sign-out>
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
       <router-link to="/chart">Chart</router-link>
     </div>
     <router-view />
