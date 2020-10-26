0514cdee7ebd66864c37335b70394e5d6af29f92
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-10-27 06:17:48 +0900

    csv読み込みのlambda関数を定義した

--- cdk/financial-viewer/lib/financial-viewer-stack.ts
+++ cdk/financial-viewer/lib/financial-viewer-stack.ts
@@ -1,9 +1,15 @@
-import * as cdk from '@aws-cdk/core';
+import * as core from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
 
-export class FinancialViewerStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class FinancialViewerStack extends core.Stack {
+  constructor(scope: core.Construct, id: string, props?: core.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const incomeStatementCsvReder = new lambda.Function(this, 'IncomeStatementCsvReader', {
+      runtime: lambda.Runtime.JAVA_11,
+      code: lambda.Code.fromAsset('../../backend/build/distributions/financial-viewer-1.0-SNAPSHOT.zip'),
+      handler: 'com.azkz.lambda.function.IncomeStatementCsvReadHandler'
+    })
+
   }
 }
