5f3dfa659d5400f3d718ae3e29b3414262ca9a59
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-11-03 06:08:56 +0900

    Cognitoの設定を記述し、認証機能を実装した。

--- frontend/financial-viewer/src/main.ts
+++ frontend/financial-viewer/src/main.ts
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+
+Amplify.configure({
+  Auth: {
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: 'ap-northeast-1_VKzuy25rR',
+
+    // REQUIRED - Amazon Cognito Region
+    region: 'ap-northeast-1',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'ap-northeast-1_VKzuy25rR',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: '2p99jiiagij7jjnfe0ifkhn11d'
+  }
+})
 
 Vue.config.productionTip = false
 
