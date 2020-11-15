a0d06a416106d7197aa91f4a89f3b293d7d0864a
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-11-16 07:40:54 +0900

    CognitoのユーザープールとIAMロールを新規作成した。

--- cdk/financial-viewer/lib/financial-viewer-stack.ts
+++ cdk/financial-viewer/lib/financial-viewer-stack.ts
@@ -3,11 +3,15 @@ import * as lambda from '@aws-cdk/aws-lambda'
 import * as event from '@aws-cdk/aws-lambda-event-sources';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as iam from '@aws-cdk/aws-iam'
+import * as cognito from '@aws-cdk/aws-cognito'
+import { Duration } from '@aws-cdk/core';
+import { ManagedPolicy } from '@aws-cdk/aws-iam';
 
 export class FinancialViewerStack extends core.Stack {
   constructor(scope: core.Construct, id: string, props?: core.StackProps) {
     super(scope, id, props);
 
+    /* ====================== S3 ====================== */
     // S3バケット
     const storage = new s3.Bucket(this, 'FinancialViewerStorage', {
       // TODO:Handlerで復号を実装したら、暗号化を有効にする
@@ -21,6 +25,7 @@ export class FinancialViewerStack extends core.Stack {
       filters: [{ suffix: '.csv' }]
     })
 
+    /* ====================== Lambda ====================== */
     // Lambda実行用ロール
     const lambdaRole = new iam.Role(this, 'LambdaRole', {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
@@ -33,13 +38,50 @@ export class FinancialViewerStack extends core.Stack {
     // Lambda関数 損益計算書CSV読み込み
     const incomeStatementCsvReder = new lambda.Function(this, 'IncomeStatementCsvReader', {
       runtime: lambda.Runtime.JAVA_11,
-      code: lambda.Code.fromAsset('../../backend/build/distributions/financial-viewer-1.0-SNAPSHOT.zip'),
+      code: lambda.Code.fromAsset('../../backend/financial-viewer/build/distributions/financial-viewer-1.0-SNAPSHOT.zip'),
       handler: 'com.azkz.lambda.handler.IncomeStatementCsvReadHandler',
       role: lambdaRole,
       events: [s3EventObjectCreated],
       memorySize: 192,
       timeout: core.Duration.minutes(1)
     })
+
+    /* ====================== Cognito ====================== */
+    // アプリユーザーのロール
+    // TODO:もし、ロールとUserPoolの紐付をコードでできるなら、その方法に変更する。※現在はコンソールで手動割当
+    // TODO:S3フルアクセスではなく、適切なポリシーを作成して付与する。
+    const userRole = new iam.Role(this, 'FinancialViewerUserRole', {
+      assumedBy: new iam.WebIdentityPrincipal('cognito-identity.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess')
+      ]
+    })
+
+    // User Pool
+    const userPool = new cognito.UserPool(this,'FinancialViewerUserPool',{
+      userPoolName:'FinancialViewer-UserPool',
+      selfSignUpEnabled:false,
+      signInAliases:{
+        email:true,
+      },
+      standardAttributes:{
+        fullname:{
+          required:true,
+          mutable:true,
+        },
+      },
+      passwordPolicy:{
+        minLength:8,
+        requireDigits:true,
+        requireLowercase:true,
+        requireUppercase:true,
+        requireSymbols:true,
+      },
+      accountRecovery:cognito.AccountRecovery.EMAIL_ONLY
+    })
+
+    const appCliant = userPool.addClient('FinancialViewerAppCliant')
+    const cliantId = appCliant.userPoolClientId
 
   }
 }
